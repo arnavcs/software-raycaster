14c5c9da46dff40b7601000c1293e07270661e5e
Arnav Kumar <arnav.kumar.sri@gmail.com>
2024-09-29 10:52:17 -0400

    column drawing given distance

--- raycaster.js
+++ raycaster.js
@@ -1,5 +1,7 @@
 console.log("loaded raycaster")
 
+const HEIGHT_SCALE = 100;
+
 function svtimes (scalar, vector) {
   return vector.map(a => scalar * a);
 }
@@ -8,25 +10,40 @@ function vvplus (vector1, vector2) {
   return vector1.map((a, i) => (a + vector2[i]));
 }
 
+function colourToString (colour) {
+  return "rgb(" + colour[0] + ", " + colour[1] + ", " + colour[2] + ")"
+}
+
 function putPixel (ctx, x, y, c) {
   ctx.fillStyle = c;
   ctx.fillRect(x, y, 1, 1);
 }
 
+// digital differential analysis
+// returns distance to collision or Infinity
+function dda (map, pos, ray) {
+  return 1;
+}
+
 function renderCol (ctx, col, map, camera, screen) {
-  cameraX = 2 * col / (screen.width - 1) - 1;
-  ray = vvplus(camera.dir, svtimes(cameraX, camera.plane));
+  let cameraX = 2 * col / (screen.width - 1) - 1;
+  let ray = vvplus(camera.dir, svtimes(cameraX, camera.plane));
+  let collision = dda(map, camera.pos, ray);
+  let wallHeight = HEIGHT_SCALE / collision;
   for (let row = 0; row < screen.height; row++) {
-    putPixel(ctx, col, row, "blue");
+    let colour = [0, 0, 0];
+    if (Math.abs(row - (screen.height - 1) / 2) < wallHeight / 2) {
+      colour = [0, 0, 255];
+    }
+    putPixel(ctx, col, row, colourToString(colour));
   }
 }
 
-// map is a 2d array of integers: 0 is nothing, 1 is wall
 function render (map, camera, screen) {
   const ctx = screen.getContext("2d");
 
   for (let col = 0; col < screen.width; col++) {
-    renderCol(ctx, col, map, camera, screen)
+    renderCol(ctx, col, map, camera, screen);
   }
 }
 
@@ -42,5 +59,5 @@ window.onload = function () {
         ,{pos: [1.5, 1.5],
           dir: [1, 2],
           plane: [2, -1]}
-        ,document.getElementById("screen"))
+        ,document.getElementById("screen"));
 }
