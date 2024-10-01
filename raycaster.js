785115623d4316a9f0672da0e3cfb38955284b5d
Arnav Kumar <arnav.kumar.sri@gmail.com>
2024-10-01 09:42:29 -0400

    lighting and cyclindrical view

--- raycaster.js
+++ raycaster.js
@@ -8,8 +8,18 @@ function vvplus (vector1, vector2) {
   return vector1.map((a, i) => (a + vector2[i]));
 }
 
+function vdot (vector1, vector2) {
+  return vector1.map((a, i) => (a * vector2[i])).reduce((acc, val) => (acc + val), 0);
+}
+
 function vmagnitude (vector) {
   return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1])
+}
+
+function vrotate (vector, angle) {
+  angle = Math.PI * -angle / 180;
+  return [vector[0] * Math.cos(angle) - vector[1] * Math.sin(angle)
+         ,vector[0] * Math.sin(angle) + vector[1] * Math.cos(angle)];
 }
 
 function mapHas (map, idx) {
@@ -22,7 +32,8 @@ function mapAt (map, idx) {
 }
 
 function colourToString (colour) {
-  return "rgb(" + colour[0] + ", " + colour[1] + ", " + colour[2] + ")";
+  let clamp = (a) => (Math.max(Math.min(a, 255), 0))
+  return "rgb(" + clamp(colour[0]) + ", " + clamp(colour[1]) + ", " + clamp(colour[2]) + ")";
 }
 
 function putPixel (ctx, x, y, c) {
@@ -31,7 +42,7 @@ function putPixel (ctx, x, y, c) {
 }
 
 // digital differential analysis
-// returns distance to collision or Infinity
+// returns [distance to collision or Infinity, direction into wall collided]
 function dda (map, pos, ray) {
   let currentSquare = [Math.floor(pos[0]), Math.floor(pos[1])];
 
@@ -47,42 +58,49 @@ function dda (map, pos, ray) {
   let stepY = 1 / Math.abs(ray[1]);
 
   let distance = 0;
+  let direction = [0, 0];
 
   while (true) {
     if (!mapHas(map, currentSquare)) {
-      return Infinity;
+      return [Infinity, [0, 0]];
     }
 
     if (mapAt(map, currentSquare)) {
-      return vmagnitude(ray) * distance;
+      return [vmagnitude(ray) * distance, direction];
     }
 
     if (nextX < nextY) {
       distance = nextX;
       nextX += stepX;
+      direction = ray[0] < 0 ? [-1, 0] : [1, 0];
       currentSquare[0] += ray[0] < 0 ? -1 : 1;
     } else {
       distance = nextY;
       nextY += stepY;
+      direction = ray[1] < 0 ? [0, -1] : [0, 1];
       currentSquare[1] += ray[1] < 0 ? -1 : 1;
     }
   }
 }
 
 function renderCol (ctx, col, map, camera, screen, options) {
-  let cameraX = 2 * col / (screen.width - 1) - 1;
-  let ray = vvplus(camera.dir, svtimes(cameraX, camera.plane));
+  let cameraX = 2 * col / (screen.width - 1) - 1; // in [-1, 1]
+  let ray = camera.plane 
+          ? vvplus(camera.dir, svtimes(cameraX, camera.plane)) 
+          : vrotate(camera.dir, cameraX * camera.range / 2); 
   let collision = dda(map, camera.pos, ray);
-  let wallHeight = screen.height / collision;
+  let wallHeight = screen.height / collision[0];
   for (let row = 0; row < screen.height; row++) {
     let colour = [0, 0, 0];
     if (Math.abs(row - (screen.height - 1) / 2) < wallHeight / 2) {
-      colour = [0, 0, 255 - options.fade * collision];
+      colour = [0, 0, 255 - options.fade * collision[0]];
+      colour = vvplus(svtimes(vdot(collision[1], options.light), [1, 1, 1]), colour)
     }
     putPixel(ctx, col, row, colourToString(colour));
   }
 }
 
+// camera has pos: vector, dir: vector, and (range: angle | plane: vector)
 function render (map, camera, screen, options) {
   const ctx = screen.getContext("2d");
 
@@ -102,7 +120,8 @@ window.onload = function () {
           [1, 1, 1, 1, 1, 1, 1, 1]]
         ,{pos: [1.5, 1.5],
           dir: [1, 2],
-          plane: [2, -1]}
+          range: 80}
         ,document.getElementById("screen")
-        ,{fade: 20});
+        ,{fade: 20,
+          light: [30, -30]});
 }
