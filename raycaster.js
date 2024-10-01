fff807d4752df9c804348b888a174deebd31cb99
Arnav Kumar <arnav.kumar.sri@gmail.com>
2024-09-30 23:14:45 -0400

    dda algorithm

--- raycaster.js
+++ raycaster.js
@@ -1,7 +1,5 @@
 console.log("loaded raycaster")
 
-const HEIGHT_SCALE = 100;
-
 function svtimes (scalar, vector) {
   return vector.map(a => scalar * a);
 }
@@ -10,8 +8,21 @@ function vvplus (vector1, vector2) {
   return vector1.map((a, i) => (a + vector2[i]));
 }
 
+function vmagnitude (vector) {
+  return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1])
+}
+
+function mapHas (map, idx) {
+  return 0 <= idx[1] && idx[1] < map.length
+      && 0 <= idx[0] && idx[0] < map[map.length - 1 - idx[1]].length;
+}
+
+function mapAt (map, idx) {
+  return map[map.length - 1 - idx[1]][idx[0]];
+}
+
 function colourToString (colour) {
-  return "rgb(" + colour[0] + ", " + colour[1] + ", " + colour[2] + ")"
+  return "rgb(" + colour[0] + ", " + colour[1] + ", " + colour[2] + ")";
 }
 
 function putPixel (ctx, x, y, c) {
@@ -22,14 +33,47 @@ function putPixel (ctx, x, y, c) {
 // digital differential analysis
 // returns distance to collision or Infinity
 function dda (map, pos, ray) {
-  return 1;
+  let currentSquare = [Math.floor(pos[0]), Math.floor(pos[1])];
+
+  // all of the following are using units of rays
+  // need to multiply by the length of the ray at the end to get length
+  let nextX = ray[0] < 0
+            ? (pos[0] - currentSquare[0]) / (- ray[0])
+            : (currentSquare[0] + 1 - pos[0]) / (ray[0]);
+  let nextY = ray[1] < 0
+            ? (pos[1] - currentSquare[1]) / (- ray[1])
+            : (currentSquare[1] + 1 - pos[1]) / (ray[1]);
+  let stepX = 1 / Math.abs(ray[0]);
+  let stepY = 1 / Math.abs(ray[1]);
+
+  let distance = 0;
+
+  while (true) {
+    if (!mapHas(map, currentSquare)) {
+      return Infinity;
+    }
+
+    if (mapAt(map, currentSquare)) {
+      return vmagnitude(ray) * distance;
+    }
+
+    if (nextX < nextY) {
+      distance = nextX;
+      nextX += stepX;
+      currentSquare[0] += ray[0] < 0 ? -1 : 1;
+    } else {
+      distance = nextY;
+      nextY += stepY;
+      currentSquare[1] += ray[1] < 0 ? -1 : 1;
+    }
+  }
 }
 
 function renderCol (ctx, col, map, camera, screen) {
   let cameraX = 2 * col / (screen.width - 1) - 1;
   let ray = vvplus(camera.dir, svtimes(cameraX, camera.plane));
   let collision = dda(map, camera.pos, ray);
-  let wallHeight = HEIGHT_SCALE / collision;
+  let wallHeight = screen.height / collision;
   for (let row = 0; row < screen.height; row++) {
     let colour = [0, 0, 0];
     if (Math.abs(row - (screen.height - 1) / 2) < wallHeight / 2) {
